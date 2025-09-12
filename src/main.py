1dff0ac1aa69228314ff281bf069488669e77f2b
Ana Zobec <ana12zobec@gmail.com>
2025-09-12 18:42:20 +0200

    Switch to argparse
    Switch to argparse instead of direct sys.argv manipulation

--- src/main.py
+++ src/main.py
@@ -1,26 +1,57 @@
 import sys
+import argparse
 
 from src.tools.show_issue import show_issue
-
-
-def print_help() -> None:
-    print("Usefull tools to use with your git project", end="")
-    print("\n")
-    print("USAGE:")
-    print("  git-tools [--help|-h|TOOLS]")
-    print("  git-tools <tool> [--help|-h]", end="")
-    print("\n")
-    print("TOOLS:")
-    print("  show-issue\t Show git issue description in your terminal")
+from src.utils.services import GitServiceType
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1 or sys.argv[1] in ("--help", "-h"):
-        print_help()
-        sys.exit(0)
+    available_git_services = ", ".join(GitServiceType.list_values())
+    parser = argparse.ArgumentParser(
+        prog="git-tools",
+        usage="%(prog)s [--help|options]",
+        description="Useful tools to use with your git project",
+    )
 
-    if len(sys.argv) >= 2 and sys.argv[1] == "show-issue":
-        exit_code = show_issue(argv=sys.argv[1:])
-        sys.exit(exit_code)
+    subparsers = parser.add_subparsers(help="--help", required=True)
+
+    parser_show_issue = subparsers.add_parser(
+        "show-issue",
+        help="Show git issue description in your terminal",
+        description="Show git issue description in your terminal",
+    )
+    parser_show_issue.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        default="gitlab",
+        choices=GitServiceType.list_values(),
+        help=f"Git service type. Available types are {available_git_services} (default: gitlab)",
+        required=False,
+    )
+    parser_show_issue.add_argument(
+        "-r",
+        "--raw",
+        action="store_true",
+        help="Print issue description in raw (non-rendered) markdown if set",
+        required=False,
+    )
+    parser_show_issue.add_argument(
+        "issue_reference",
+        type=int,
+        help="Reference number of the issue to get description of",
+    )
+    parser_show_issue.set_defaults(func=show_issue)
+
+    args = parser.parse_args()
+    args.func(args)
+
+    # if len(sys.argv) == 1 or sys.argv[1] in ("--help", "-h"):
+    #     print_help()
+    #     sys.exit(0)
+    #
+    # if len(sys.argv) >= 2 and sys.argv[1] == "show-issue":
+    #     exit_code = show_issue(argv=sys.argv[1:])
+    #     sys.exit(exit_code)
 
     sys.exit(0)
--- src/tools/show_issue.py
+++ src/tools/show_issue.py
@@ -7,51 +7,14 @@ from src.utils.services import GitServiceType, GitLabService
 DEBUG = False
 
 
-def print_help(available_git_services: str, default_git_service_type: str):
-    print("Show Git issue description in your terminal", end="")
-    print("\n")
-    print("USAGE:")
-    print("  git-tools show-issue [--help|-h|--type|-t] <issue_number> [--raw|-r]", end="")
-    print("\n")
-    print("OPTIONS:")
-    print("  --help, -h\t Show this help.")
-    print("  --raw,  -r\t Print issue description without markdown rendering.")
-    print(
-        f"  --type, -t\t Choose git service type. Available types are: {available_git_services}; Default is: {default_git_service_type}"
-    )
+def show_issue(args) -> int:
+    git_service_type = args.type
+    is_raw_text = args.raw
+    issue_reference = args.issue_reference
 
-
-def show_issue(argv: list) -> int:
-    git_service_type = "gitlab"
-    available_git_services = ", ".join(GitServiceType.list_values())
-
-    if len(argv) == 1:
-        print("Error: Invalid use of the tool. Use options --help or -h to see usage")
-        return 1
-
-    if len(argv) >= 2 and argv[1] in ("--help", "-h"):
-        print_help(available_git_services=available_git_services, default_git_service_type=git_service_type)
+    if git_service_type == GitServiceType.GITHUB.value:
+        print("Not yet implemented. Use other available git services.")
         return 0
-
-    if len(argv) == 4 and argv[1] in ("--type", "-t"):
-        git_service_type = argv[2]
-        if not GitServiceType.has_value(git_service_type):
-            print(f"Error: Git service {git_service_type} not supported.")
-            print(f"Supported types are: {available_git_services}")
-            return 1
-        if git_service_type == GitServiceType.GITHUB.value:
-            print("Not yet implemented. Use other available git services.")
-            return 0
-        issue_reference = int(argv[3])
-
-    if len(argv) >= 2:
-        try:
-            issue_reference = int(argv[1])
-        except ValueError as ve:
-            print(f"Error: Issue reference must be a number: {ve}")
-            return 1
-
-    is_raw_text = "--raw" in argv or "-r" in argv
 
     git_project_info, exit_code = GitConfig.get_git_project_info()
     if exit_code != 0:
@@ -80,13 +43,13 @@ def show_issue(argv: list) -> int:
 
     raw_labels = "Labels: "
     for label in gitlab_project.issue.labels:
-        raw_labels += f"**_{label}_** "
+        raw_labels += f"**[{label}]** "
 
     # Print raw markdown
     if is_raw_text:
         print(f"# {gitlab_project.issue.title}", end="")
         print("\n")
-        print(gitlab_project.issue.description, end="")
+        print(gitlab_project.issue.description, end="\n")
         print("---")
         print(raw_labels)
         return 0
